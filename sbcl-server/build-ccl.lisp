edfe5d980366d67c69eeee4390761c28757ec607
em7 <mm007.emko@gmail.com>
2024-03-23 18:12:51 +0100

    CCL build, removed :gui from SBCL build

--- sbcl-server/build-ccl.lisp
+++ sbcl-server/build-ccl.lisp
@@ -2,17 +2,16 @@
 (ql:quickload :sbcl-server)
 
 (defun main ()
-  (format t "Starting server at port 8080~%")
   (sbcl-server:start-web-server)
   (bt:join-thread (find-if (lambda (th)
                             (search "hunchentoot" (bt:thread-name th)))
                           (bt:all-threads))))
 
-(sb-ext:save-lisp-and-die "my-web-server.exe"
-                          :executable t
-                          :toplevel #'main
-                          :compression nil)
+(save-application "my-web-server.exe"
+                  :prepend-kernel t
+                  :error-handler :quit
+                  :application-type :gui
+                  :toplevel-function #'main)
 
 ;; build with:
-;; sbcl --disable-debugger --load build.lisp
-6:08 PM 23-Mar-24
+;; ccl --load build.lisp
